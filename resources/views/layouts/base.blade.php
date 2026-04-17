a32727ebe41e31aa53fb5c734f89ab7179e5da7f
Developer <dev@example.com>
2026-04-17 19:50:11 +0200

    Entferne unnötigen Abstand zwischen Navigation und Hauptinhalt
    - Entferne py-6 Padding aus layouts/base.blade.php
    - Navigation sitzt jetzt direkt am Inhalt ohne großen vertikalen Abstand
    - Verbessert die Raumnutzung und das visuelle Erscheinungsbild

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -24,7 +24,7 @@
 
             <!-- Page Content -->
             <main class="flex-1">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     @yield('content')
                 </div>
             </main>
